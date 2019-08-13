0c93467b79fc0303b7937a5b1a893a0b3bfbf1f0
Simingchen <siming535@gmail.com>
2019-08-13 17:55:17 +0800

    基于vue-cli 处理ssr

---
+++ vue-cli-ssr/build/webpack.server.conf.js
@@ -0,0 +1,31 @@
+/* 介紹地址
+https://segmentfault.com/a/1190000014733669
+ */
+const webpack = require("webpack");
+const merge = require("webpack-merge");
+const base = require("./webpack.base.conf");
+const webapckNodeExternals = require("webpack-node-externals");
+
+
+const vueSSRServerPlugin = require("vue-server-renderer/server-plugin");
+
+module.exports = merge(base,{
+    //  告知webpack，需要在node端运行
+    target:"node",
+    entry:"./src/entry-server.js",
+    devtool:"source-map",
+    output:{
+        filename:'server-buldle.js',
+        libraryTarget: "commonjs2"
+    },
+    externals:[
+        webapckNodeExternals()
+    ],
+    plugins:[
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV':'"devlopment"',
+            'process.ent.VUE_ENV': '"server"'
+        }),
+        new vueSSRServerPlugin()
+    ]
+});
